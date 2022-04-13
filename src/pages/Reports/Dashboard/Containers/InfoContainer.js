4b6da8106b5c2472630535f4fff53e6418816c7e
jgachunga <johnkachunga@gmail.com>
2022-04-13 12:18:47 +0300

    event media reducer store

--- src/pages/Reports/Dashboard/Containers/InfoContainer.js
+++ src/pages/Reports/Dashboard/Containers/InfoContainer.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { useNavigate, useParams } from 'react-router-dom';
 import { Card, Row, Col, CardText, CardSubtitle, Button } from 'reactstrap';
-import { getInSituMedia } from '../../../../store/dashboard/action';
+import { getInSituMedia } from '../../../../store/events/action';
 import { formatDate } from '../../../../store/utility';
 
 const InfoContainer = () => {
