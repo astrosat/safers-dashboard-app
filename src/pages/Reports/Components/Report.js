6ef54566c16c340314600bfdc10f581e2959b480
stevefaeembra <stevendkay@gmail.com>
2022-06-23 11:40:33 +0100

    feat(frontend): Remove favourites cell
    Confirmed that this functionality not going to happen,
    so removed dead code
    
    IssueID SAFB-173

--- src/pages/Reports/Components/Report.js
+++ src/pages/Reports/Components/Report.js
@@ -20,19 +20,6 @@ const Report = ({ card, alertId, setSelectedAlert }) => {
       <CardBody className='p-0 m-2'>
         
         <Row className='mt-2'>
-          {/* <Col md={1}>
-            <button
-              type="button"
-              className="btn float-start py-0 px-1"
-              aria-label='report-favorite-button'
-              onClick={(e) => {
-                e.stopPropagation();
-                setFavorite(card.id);
-              }}
-            >
-              <i className={`mdi mdi-star${!card.isFavorite ? '-outline' : ''} card-title`}></i>
-            </button> 
-          </Col> */}
           <Col>
             <Row>
               <Col>
